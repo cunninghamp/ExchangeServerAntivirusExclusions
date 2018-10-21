1d95fcdeb7fd7ada248bcdd483c3bac1ac58ed31
Paul Cunningham <paul@locklan.com.au>
2018-10-22 08:37:47 +1000

    Update Get-Exchange2016AVExclusions.ps1

--- Get-Exchange2016AVExclusions.ps1
+++ Get-Exchange2016AVExclusions.ps1
@@ -30,25 +30,19 @@ Written by: Paul Cunningham
 
 Find me on:
 
-* My Blog:	http://paulcunningham.me
+* My Blog:	https://paulcunningham.me
 * Twitter:	https://twitter.com/paulcunningham
-* LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+* LinkedIn:	https://au.linkedin.com/in/cunninghamp/
 * Github:	https://github.com/cunninghamp
 
 Additional credit:
 
 * Matt K (via http://exchangeserverpro.com/powershell-script-exchange-2013-antivirus-exclusions/#comment-244497)
 
-For more Exchange Server tips, tricks and news
-check out Exchange Server Pro.
-
-* Website:	http://exchangeserverpro.com
-* Twitter:	http://twitter.com/exchservpro
-
-Change Log
+Change Log:
 V1.00, 05/05/2016 - Initial version
 
-
+License:
 
 The MIT License (MIT)
 
